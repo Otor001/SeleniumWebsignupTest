5137131bf483bd86f091bd01699f48765d9e3bcb
Otor001 <126812455+Otor001@users.noreply.github.com>
2023-03-30 22:55:30 +0100

    C:\Users\Public\Downloads\FirstJavaProject

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world. This is my project");
+   System.out.println("I am from cross river state, nigeria");
+   System.out.println("I went to airforce secondary school ikeja, lagos state, nigeria");
+   System.out.println("I had my first degree at cross river state university calabar, nigeria");
+   System.out.println("I am an accountant by degree");
+   System.out.println("I recently switched a career into a software enginering");
+   System.out.println("I love adventures");
+   System.out.println("I love to travel around the world");
+   System.out.println("I love a team sport like football, volleyball, and basketball");
+   System.out.println("My career job is to be a software tester");
+    }
+}
